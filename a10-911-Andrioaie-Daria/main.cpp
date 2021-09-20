75212a09d59a66f29522f809a82176cb488ab217
911-Andrioaie-Daria <72091687+911-Andrioaie-Daria@users.noreply.github.com>
2021-09-20 21:35:04 +0300

    Add files via upload

---
+++ a10-911-Andrioaie-Daria/main.cpp
@@ -0,0 +1,38 @@
+//
+// Created by daria on 21/03/2021.
+//
+
+#include "Tests/TestAll.h"
+#include "FileRepository.h"
+#include "DogValidator.h"
+#include "AdminService.h"
+#include "UserService.h"
+#include "ConsoleUI.h"
+#include "AdminGUI.h"
+#include "GUI.h"
+#include "UserGUI.h"
+#include <crtdbg.h>
+
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication a{ argc, argv };
+
+    //run_all_tests();
+
+    FileRepository repository(R"(C:\Users\daria\Documents\study\facultate\anul 1\semestrul 2\OOP\a10-911-Andrioaie-Daria-1\Resources\dogs.txt)");
+
+    DogValidator validator;
+
+    AdminService adminService{repository, validator};
+    UserService userService{repository};
+
+//    UserInterface userInterface{adminService, userService};
+//    userInterface.start_program();
+
+    GUI gui{adminService, userService, nullptr};
+    gui.show();
+    _CrtDumpMemoryLeaks();
+    return a.exec();
+}
